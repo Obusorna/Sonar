2a3dd45a9e0f4e0e1f41f6363c0cd86a47acbb19
Obusorna <60769065+Obusorna@users.noreply.github.com>
2020-03-02 18:20:46 +0530

    Update Test1.php

--- skeleton-1.x/src/Test1.php
+++ skeleton-1.x/src/Test1.php
@@ -1,1 +1,25 @@
 
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
